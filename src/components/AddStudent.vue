b4c67758a875cdadedbc5fd7ec3c075c188b439d
ZachyDev <mosetizachary001@gmail.com>
2020-09-29 10:30:14 +0300

    Started Vue-authetication project and developed the mark up✅✅

---
+++ src/components/AddStudent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Studa
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddStudent',
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/GetStudents.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      GetStudents
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GetStudents',
+};
+</script>
+
+<style>
+
+</style>
